9e89e325a3e531198873f234e6874c5eb6c48894
jarvis1807 <72394122+jarvis1807@users.noreply.github.com>
2020-12-15 16:49:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var syncball, database, position;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+
+    syncball = createSprite(250,250,10,10);
+    syncball.shapeColor = "red";
+
+    var refPosition = database.ref('ball/position');
+    refPosition.on('value', readPosition, showError)
+}
+ 
+
+function draw(){
+    background("white");
+    if(position !== undefined){
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+}
+
+function changePosition(x,y){
+    database.ref('ball/position').set({'x':position.x+x, 'y':position.y+y})
+}
+
+function readPosition(data){
+    position = data.val();
+    syncball.x = position.x
+    syncball.y = position.y
+
+}
+
+function showError(){
+    console.log("error")
+}
